76ae829fb18b39e712efcdf8c6f0c9e40f99fc40
Abhi <abhinav118@gmail.com>
2025-08-20 12:17:30 -0400

    Add components/TaskCard.tsx

---
+++ components/TaskCard.tsx
@@ -0,0 +1,14 @@
+interface TaskCardProps {
+  title?: any
+  status?: any
+  dueDate?: any
+  clickable?: any
+}
+
+export default function TaskCard({ title, status, dueDate, clickable }: TaskCardProps) {
+  return (
+    <div className="">
+      {/* TaskCard component content */}
+    </div>
+  )
+}
