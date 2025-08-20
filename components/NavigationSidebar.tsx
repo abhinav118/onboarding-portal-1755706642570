81ea0583752a01a975332283f8f3af4c353bd649
Abhi <abhinav118@gmail.com>
2025-08-20 12:17:31 -0400

    Add components/NavigationSidebar.tsx

---
+++ components/NavigationSidebar.tsx
@@ -0,0 +1,12 @@
+interface NavigationSidebarProps {
+  collapsible?: any
+  width?: any
+}
+
+export default function NavigationSidebar({ collapsible, width }: NavigationSidebarProps) {
+  return (
+    <div className="">
+      {/* NavigationSidebar component content */}
+    </div>
+  )
+}
